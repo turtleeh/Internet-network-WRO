7298c6b232d4d8156e2e55e57d487af8f36eb028
turtleeh <128096495+turtleeh@users.noreply.github.com>
2023-09-12 01:30:54 +0200

    Update shortest path algorithm.ino

--- shortest path algorithm.ino
+++ shortest path algorithm.ino
@@ -10,7 +10,7 @@ struct vertex{
   Vector<int> connections;
   Vector<float> weight;
   float distance;
-  int previous;
+  int previous = infinite;
   bool visited;
   };
  
@@ -24,34 +24,42 @@ vertex minimumDistance(Vector<vertex> list){
 } 
  
  
-Vector<vertex> dijkstra(Vector<vertex> G, vertex S, vertex F){
+Vector<int> dijkstra(Vector<vertex> G,const int S,const int F){
     Vector<vertex> Q;
     for (int i; i<G.size(); i++){
         G[i].distance = infinite;
-        if (G[i].ID != S.ID) {
+        if (G[i].ID != S) {
           Q.push_back(G[i]);
           G[i].visited=false;}
  
     }
-    S.distance = 0;
- 
+    G[S].distance = 0;
+    G[S].previous = infinite;
     while (Q.size()){
         vertex U = minimumDistance(Q);
         G[U.ID].visited=true;
         for (int i; i<U.connections.size(); i++)
           if (!G[U.connections[i]].visited){
             float tempDistance = U.distance + U.weight[i];
-            if (tempDistance < G[U.connections[i]].distance)
+            if (tempDistance < G[U.connections[i]].distance){
                 G[U.connections[i]].distance = tempDistance;
                 G[U.connections[i]].previous = U.ID;
-          }
-}
-return G;
+          }}}
+            Serial.println("while");
+  Vector<int>re;
+  re.push_back(F);
+    Serial.println(re[0]);
+    
+    Serial.println(F);
+  while (re[-1] != S)
+    re.push_back(G[re[-1]].previous);
+  Serial.println("re");
+  return re;
 }
 void setup(){
   Serial.begin(9600);
  
- 
+
   Vector<vertex> graph;
   vertex placeholder;
   placeholder.connections.clear();
@@ -62,7 +70,7 @@ void setup(){
   placeholder.weight.push_back(2);
   placeholder.weight.push_back(5);
   graph.push_back(placeholder);
- 
+  
   placeholder.connections.clear();
   placeholder.weight.clear();
   placeholder.ID = 1;
@@ -71,7 +79,7 @@ void setup(){
   placeholder.weight.push_back(3);
   placeholder.weight.push_back(4);
   graph.push_back(placeholder);
- 
+
   placeholder.connections.clear();
   placeholder.weight.clear();
   placeholder.ID = 2;
@@ -80,7 +88,6 @@ void setup(){
   placeholder.weight.push_back(3);
   placeholder.weight.push_back(2);
   graph.push_back(placeholder);
- 
   placeholder.connections.clear();
   placeholder.weight.clear();
   placeholder.ID = 3;
@@ -89,11 +96,13 @@ void setup(){
   placeholder.weight.push_back(4);
   placeholder.weight.push_back(5);
   graph.push_back(placeholder);
- 
-  graph=dijkstra(graph, graph[0], graph[1]);
-  Serial.print(graph[1].previous);
-  Serial.print(graph[graph[1].previous].previous);
-  Serial.print(graph[graph[graph[1].previous].previous].previous);
+  Vector<int> steps = dijkstra(graph,0,1);
+    Serial.println("functions");
+      Serial.println(steps.size());
+  for(int i; i< steps.size(); i++){
+    Serial.println(steps[i]);
+  }
+
 }
 void loop() {
   }
