97db4de413237e743a6798000a99781fe5a80649
turtleeh <128096495+turtleeh@users.noreply.github.com>
2023-09-13 12:24:26 +0200

    Update shortest path algorithm.ino

--- shortest path algorithm.ino
+++ shortest path algorithm.ino
@@ -1,108 +1,112 @@
-#include <Vector.h>
- 
+#include <tiny-collections.h>
+
+#include <ArduinoSTL.h>
+
+#include <Arduino_AVRSTL.h>
+
+
+
+
 #define infinite 999
- 
-int neighbors[2] = {1,3};
-int distances[2] = {10,5};
- 
-struct vertex{
-  int ID;
-  Vector<int> connections;
-  Vector<float> weight;
-  float distance;
-  int previous = infinite;
-  bool visited;
-  };
- 
- 
-vertex minimumDistance(Vector<vertex> list){
-  vertex min = list[1];
-  for (int i; i<list.size();i++)
-    if (list[i].distance<min.distance)
-      min=list[i];
-  return min;
-} 
- 
- 
-Vector<int> dijkstra(Vector<vertex> G,const int S,const int F){
-    Vector<vertex> Q;
-    for (int i; i<G.size(); i++){
+
+struct vertex {
+    int ID;
+    vector<int> connections;
+    vector<float> weight;
+    float distance;
+    int previous = -1;
+    bool visited=0;
+};
+
+
+int minimumDistance(vector<vertex> list,vector<int> selection) {
+    int min = list[selection[0]].ID;
+    for (int i = 0; i < selection.size(); i++)
+        if (list[selection[i]].distance < list[min].distance) {
+            min = list[i].ID;
+        }
+    return min;
+}
+
+
+vector<int> dijkstra(vector<vertex> G, const int S, const int F) {
+    vector<int> Q;
+    for (int i = 0; i < G.size(); i++) {
         G[i].distance = infinite;
-        if (G[i].ID != S) {
-          Q.push_back(G[i]);
-          G[i].visited=false;}
- 
-    }
+        Q.push_back(G[i].ID);
+        G[i].visited = false;
+        }
+
+    
     G[S].distance = 0;
     G[S].previous = infinite;
-    while (Q.size()){
-        vertex U = minimumDistance(Q);
-        G[U.ID].visited=true;
-        for (int i; i<U.connections.size(); i++)
-          if (!G[U.connections[i]].visited){
-            float tempDistance = U.distance + U.weight[i];
-            if (tempDistance < G[U.connections[i]].distance){
-                G[U.connections[i]].distance = tempDistance;
-                G[U.connections[i]].previous = U.ID;
-          }}}
-            Serial.println("while");
-  Vector<int>re;
-  re.push_back(F);
-    Serial.println(re[0]);
+    while (!G[F].visited) {
+      
+        int U = minimumDistance(G,Q);
+        G[U].visited = true;
+            Serial.print("1");
+        for (int i = 0; i < G[U].connections.size(); i++)
+            if (!G[G[U].connections[i]].visited) {
+                float tempDistance = G[U].distance + G[U].weight[i];
+                if (tempDistance <= G[G[U].connections[i]].distance) {
+                    G[G[U].connections[i]].distance = tempDistance;
+                    G[G[U].connections[i]].previous = U;
+                    G[U].distance = infinite;
+
+                }
+            }
+    }
+    vector<int>re;
+    re.push_back(F);
+    while (re.back() != S)
+        re.push_back(G[re.back()].previous);
+    return re;
+}
+void setup() {
+    Serial.begin(9600);
+    vector<vertex> graph;
+    vertex placeholder;
     
-    Serial.println(F);
-  while (re[-1] != S)
-    re.push_back(G[re[-1]].previous);
-  Serial.println("re");
-  return re;
+    placeholder.connections.clear();
+    placeholder.weight.clear();
+    placeholder.ID = 0;
+    placeholder.connections.push_back(2);
+    placeholder.connections.push_back(3);
+    placeholder.weight.push_back(2);
+    placeholder.weight.push_back(5);
+    graph.push_back(placeholder);
+
+    placeholder.connections.clear();
+    placeholder.weight.clear();
+    placeholder.ID = 1;
+    placeholder.connections.push_back(2);
+    placeholder.connections.push_back(3);
+    placeholder.weight.push_back(3);
+    placeholder.weight.push_back(4);
+    graph.push_back(placeholder);
+
+    placeholder.connections.clear();
+    placeholder.weight.clear();q
+    placeholder.connections.push_back(1);
+    placeholder.connections.push_back(0);
+    placeholder.weight.push_back(3);
+    placeholder.weight.push_back(2);
+    graph.push_back(placeholder);
+    
+    placeholder.connections.clear();
+    placeholder.weight.clear();
+    placeholder.ID = 3;
+    placeholder.connections.push_back(1);
+    placeholder.connections.push_back(0);
+    placeholder.weight.push_back(4);
+    placeholder.weight.push_back(5);
+    graph.push_back(placeholder);
+    
+    vector<int> steps = dijkstra(graph, 0, 1);
+    
+    Serial.print(steps.size());
+    for (int i = 0; i < steps.size(); i++) {
+        Serial.print(steps[i]);
+    }
 }
-void setup(){
-  Serial.begin(9600);
- 
-
-  Vector<vertex> graph;
-  vertex placeholder;
-  placeholder.connections.clear();
-  placeholder.weight.clear();
-  placeholder.ID =0;
-  placeholder.connections.push_back(2);
-  placeholder.connections.push_back(3);
-  placeholder.weight.push_back(2);
-  placeholder.weight.push_back(5);
-  graph.push_back(placeholder);
-  
-  placeholder.connections.clear();
-  placeholder.weight.clear();
-  placeholder.ID = 1;
-  placeholder.connections.push_back(2);
-  placeholder.connections.push_back(3);
-  placeholder.weight.push_back(3);
-  placeholder.weight.push_back(4);
-  graph.push_back(placeholder);
-
-  placeholder.connections.clear();
-  placeholder.weight.clear();
-  placeholder.ID = 2;
-  placeholder.connections.push_back(1);
-  placeholder.connections.push_back(0);
-  placeholder.weight.push_back(3);
-  placeholder.weight.push_back(2);
-  graph.push_back(placeholder);
-  placeholder.connections.clear();
-  placeholder.weight.clear();
-  placeholder.ID = 3;
-  placeholder.connections.push_back(1);
-  placeholder.connections.push_back(0);
-  placeholder.weight.push_back(4);
-  placeholder.weight.push_back(5);
-  graph.push_back(placeholder);
-  Vector<int> steps = dijkstra(graph,0,1);
-    Serial.println("functions");
-      Serial.println(steps.size());
-  for(int i; i< steps.size(); i++){
-    Serial.println(steps[i]);
-  }
-
-}
-void loop() {
-  }
+void loop(){}
